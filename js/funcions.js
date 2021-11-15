9a47c9de023f3a3124439b85e898dda829043236
xperez2001 <xavierperezbdn@gmail.com>
2021-11-15 22:35:49 +0100

    Prova 4 del Login responsive

--- js/funcions.js
+++ js/funcions.js
@@ -5,28 +5,36 @@ function LogIn() {
     if (document.body.scrollWidth > 680)
     {
         if (loginHTML.style.display == "none" || loginHTML.style.display == "")
+        {
             loginHTML.style.display = "block";
+        }
         else if (loginHTML.style.display == "block")
+        {
             loginHTML.style.display = "none";
+        }
     }
     else 
     {
+        var cuerpoHTML = document.getElementById("Cuerpo");
+
         if (loginHTML.style.display == "none" || loginHTML.style.display == "")
-        {
-            var cuerpoHTML = document.getElementById("Cuerpo");
+        {          
             cuerpoHTML.style.filter = "blur(10px)"
-            cuerpoHTML.style.position = "relative";
-    
+            cuerpoHTML.style.position = "relative";  
+            loginHTML.style.zIndex = 1;
             loginHTML.style.display = "block";
             loginHTML.style.gridColumnStart = "1";
             loginHTML.style.gridColumnStart = "2";
             loginHTML.style.gridRowStart = "3";
             loginHTML.style.gridRowEnd = "4";
-            loginHTML.style.zIndex = 1;
             loginHTML.style.marginTop = "50%";
         }
         else if (loginHTML.style.display == "block")
+        {
             loginHTML.style.display = "none";
-
+            cuerpoHTML.style.filter = "blur(0px)";
+            cuerpoHTML.style.position = "block";
+            loginHTML.style.marginTop = "0";
+        }
     }
 }
