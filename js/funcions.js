391f2c5717a7a7d848bf646d96774eb62a614fd6
xperez2001 <xavierperezbdn@gmail.com>
2021-11-15 22:17:17 +0100

    Tercera prova del login responsive

--- js/funcions.js
+++ js/funcions.js
@@ -11,17 +11,22 @@ function LogIn() {
     }
     else 
     {
-        var cuerpoHTML = document.getElementById("Cuerpo");
-        cuerpoHTML.style.filter = "blur(10px)"
-        cuerpoHTML.style.position = "relative";
-
-        loginHTML.style.display = "block";
-        loginHTML.style.gridColumnStart = "1";
-        loginHTML.style.gridColumnStart = "2";
-        loginHTML.style.gridRowStart = "3";
-        loginHTML.style.gridRowEnd = "4";
-        loginHTML.style.zIndex = 1;
-        loginHTML.style.marginTop = "50%";
+        if (loginHTML.style.display == "none" || loginHTML.style.display == "")
+        {
+            var cuerpoHTML = document.getElementById("Cuerpo");
+            cuerpoHTML.style.filter = "blur(10px)"
+            cuerpoHTML.style.position = "relative";
+    
+            loginHTML.style.display = "block";
+            loginHTML.style.gridColumnStart = "1";
+            loginHTML.style.gridColumnStart = "2";
+            loginHTML.style.gridRowStart = "3";
+            loginHTML.style.gridRowEnd = "4";
+            loginHTML.style.zIndex = 1;
+            loginHTML.style.marginTop = "50%";
+        }
+        else if (loginHTML.style.display == "block")
+            loginHTML.style.display = "none";
 
     }
 }
