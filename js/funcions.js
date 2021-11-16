431017f145bfb8fd9e30636225f8d10678d03b42
xperez2001 <xavierperezbdn@gmail.com>
2021-11-16 15:53:16 +0100

    Login i Registre funciona menys al inici

--- js/funcions.js
+++ js/funcions.js
@@ -1,63 +1,66 @@
 function LogIn() {
 
-    var loginHTML = document.getElementById("LoginContainer");
+    var htmlElmenent = document.getElementById("Log");
     
     if (document.body.scrollWidth > 680)
     {
-        if (loginHTML.style.display == "none" || loginHTML.style.display == "")
+        if (htmlElmenent.style.display == "none" || htmlElmenent.style.display == "")
         {
-            loginHTML.style.display = "block";
+            htmlElmenent.style.display = "block";
         }
-        else if (loginHTML.style.display == "block")
+        else if (htmlElmenent.style.display == "block")
         {
-            loginHTML.style.display = "none";
+            htmlElmenent.style.display = "none";
         }
     }
     else 
     {
         var cuerpoHTML = document.getElementById("Cuerpo");
 
-        if (loginHTML.style.display == "none" || loginHTML.style.display == "")
+        if (htmlElmenent.style.display == "none" || htmlElmenent.style.display == "")
         {          
             cuerpoHTML.style.filter = "blur(10px)"
             cuerpoHTML.style.position = "relative";  
-            loginHTML.style.zIndex = 1;
+            htmlElmenent.style.zIndex = 1;
+            htmlElmenent.style.display = "block";
+            htmlElmenent.style.gridColumnStart = "1";
+            htmlElmenent.style.gridColumnStart = "2";
+            htmlElmenent.style.gridRowStart = "3";
+            htmlElmenent.style.gridRowEnd = "4";
+            htmlElmenent.style.marginTop = "200px";
+        }
+        else if (htmlElmenent.style.display == "block")
+        {
+            htmlElmenent.style.display = "none";
+            cuerpoHTML.style.filter = "blur(0px)";
+            cuerpoHTML.style.position = "block";
+            htmlElmenent.style.marginTop = "0";
+        }
+    }
+}
+
+function LogIn2(type)
+{
+    var loginHTML = document.getElementById("Log");
+    var regHTML = document.getElementById("Reg");
+    var cuerpoHTML = document.getElementById("Cuerpo");
+
+    if (type == 1)
+    {
+        if (loginHTML.style.display == "none" || loginHTML.style.display == "")
+        {
+            cuerpoHTML.style.filter = "blur(10px)";
             loginHTML.style.display = "block";
-            loginHTML.style.gridColumnStart = "1";
-            loginHTML.style.gridColumnStart = "2";
-            loginHTML.style.gridRowStart = "3";
-            loginHTML.style.gridRowEnd = "4";
-            loginHTML.style.marginTop = "200px";
         }
         else if (loginHTML.style.display == "block")
         {
+            cuerpoHTML.style.filter = "blur(0)";
+            regHTML.style.display = "none"
             loginHTML.style.display = "none";
-            cuerpoHTML.style.filter = "blur(0px)";
-            cuerpoHTML.style.position = "block";
-            loginHTML.style.marginTop = "0";
         }
     }
-}
-
-function LogIn2()
-{
-    var loginHTML = document.getElementById("LoginContainer");
-    var cuerpoHTML = document.getElementById("Cuerpo");
-
-    if (loginHTML.style.display == "none" || loginHTML.style.display == "")
-    {          
-        cuerpoHTML.style.filter = "blur(10px)";
-        loginHTML.style.display = "block";
-    }
-    else if (loginHTML.style.display == "block")
+    else if (type == 2)
     {
-        loginHTML.style.display = "none";
-        cuerpoHTML.style.filter = "blur(0px)";
+        regHTML.style.display = "block";
     }
-
-}
-
-function Registre() {
-    var form = document.getElements
-    
 }
